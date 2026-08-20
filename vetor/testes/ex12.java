94014456582ead5fe2e7f87aa3a42d7887a4a36b
lou <louise.bc@aluno.ifsc.edu.br>
2026-08-20 20:10:00 -0300

    adição do exercício de binarySearch

--- vetor/testes/ex12.java
+++ vetor/testes/ex12.java
@@ -22,13 +22,58 @@ public class ex12 {
         Random aleatorio = new Random();
         while (vetor.obterTamanho() < quantidade) {
             int numeroSorteado = aleatorio.nextInt(range);
-            vetor.inserirOrdenadov2(numeroSorteado);
+//            vetor.inserirOrdenadov2(numeroSorteado);
+            vetor.inserir(numeroSorteado);
+        }
+
+    }
+
+    public int buscarLinear(Vetor<Integer> vetor, int alvo) {
+        for (int i = 0 ; i < vetor.obterTamanho(); i++) {
+            if (vetor.ler(i) == alvo) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int buscarLinearOrdenada(Vetor<Integer> vetor, int alvo) {
+        for (int i = 0 ; i < vetor.obterTamanho(); i++) {
+            if (vetor.ler(i) == alvo) {
+                return i;
+            } else if (vetor.ler(i) > alvo) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    public int buscarBinaria(Vetor<Integer> vetor, int alvo) {
+
+        int inicio = 0;
+        int fim = vetor.obterTamanho();
+
+        while (inicio <= fim) {
+
+            int meio = (inicio + fim)/2;
+
+            if (vetor.ler(meio) == alvo) {
+                return meio;
+            }  else if (vetor.ler(meio) > alvo) {
+                fim = meio -1;
+            } else {
+                inicio = meio + 1;
+            }
+
+
+
         }
 
 
 
-
-
-
     }
+
+
+
+
 }
